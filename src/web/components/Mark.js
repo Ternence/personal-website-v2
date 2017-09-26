3b0cefaf3308167b81af6bc506c814895a39afa2
Mark Larah <mark@larah.me>
2017-09-26 00:10:48 -0700

    container

--- src/web/components/Mark.js
+++ src/web/components/Mark.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import styled from 'styled-components';
 import Link from './Link';
 
@@ -8,33 +8,50 @@ const Title = styled.h1`
   color: palevioletred;
 `;
 
+const Container = styled.div`transform: skew(1deg);`;
+
 class Mark extends Component {
-    render() {
-        return (
-            <div>
-                <header>
-                    <h1>Mark Larah</h1>
-                    <aside><i>Developer of fine artisanal software</i></aside>
-                </header>
+  render() {
+    return (
+      <Container>
+        <header>
+          <h1>Mark Larah</h1>
+          <aside>
+            <i>Developer of fine artisanal software</i>
+          </aside>
+        </header>
 
-                <h2>48656C6C6F20546865726521</h2>
-                <p>I am a Software Engineer from the UK currently working on web infrastructure at <Link url="https://www.yelp.com">Yelp</Link>.</p>
+        <h2>48656C6C6F20546865726521</h2>
+        <p>
+          I am a Software Engineer from the UK currently working on web
+          infrastructure at <Link url="https://www.yelp.com">Yelp</Link>.
+        </p>
 
-                <h2>More Info</h2>
-                <ul>
-                    <li><Link url="https://blog.larah.me" /></li>
-                    <li><Link url="https://uk.linkedin.com/in/marklarah" /></li>
-                    <li><Link url="https://twitter.com/mark_larah" /></li>
-                    <li><Link url="https://github.com/magicmark" /></li>
-                </ul>
-          
-                <h2>Projects</h2>
-                <ul>
-                    <li><Link url="https://composerize.com/">Composerize</Link></li>
-                </ul>
-            </div>
-        )
-    }
+        <h2>More Info</h2>
+        <ul>
+          <li>
+            <Link url="https://blog.larah.me" />
+          </li>
+          <li>
+            <Link url="https://uk.linkedin.com/in/marklarah" />
+          </li>
+          <li>
+            <Link url="https://twitter.com/mark_larah" />
+          </li>
+          <li>
+            <Link url="https://github.com/magicmark" />
+          </li>
+        </ul>
+
+        <h2>Projects</h2>
+        <ul>
+          <li>
+            <Link url="https://composerize.com/">Composerize</Link>
+          </li>
+        </ul>
+      </Container>
+    );
+  }
 }
 
 export default Mark;
