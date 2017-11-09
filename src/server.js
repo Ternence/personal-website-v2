4b83c72a9c42234b3a3e3cecf594401cda890c39
Mark Larah <mark@larah.me>
2017-11-09 07:51:50 -0800

    Update server.js

--- src/server.js
+++ src/server.js
@@ -51,6 +51,9 @@ app.use('/assets', express.static(path.join(__dirname, 'web', 'public')));
 // TODO: merge static manifest and favicon with root route
 app.use('/favicon.ico', express.static(path.join(__dirname, 'web', 'public', 'favicon.ico')));
 
+app.use('/profile.jpg', express.static(path.join(__dirname, 'profile.jpg')));
+
+
 app.get('/keybase.txt', (req, res) => {
     const stream = fs.createReadStream('keybase.txt', 'utf8');
     stream.pipe(res);
