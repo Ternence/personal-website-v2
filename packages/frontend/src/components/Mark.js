33649b6de70d0a88510b8b940902112f832f11d5
Mark Larah <mark@larah.me>
2018-01-06 00:42:53 -0800

    changes

--- packages/frontend/src/components/Mark.js
+++ packages/frontend/src/components/Mark.js
@@ -2,53 +2,59 @@ import * as React from 'react';
 import styled from 'styled-components';
 import Auth from './Auth';
 import Link from './Link';
+import CookieNotice from './CookieNotice';
 
 const Container = styled.div`
-    transform: skew(1deg);
+    transform: skew(1.35deg);
 `;
 
 const Mark = () => (
-    <Container>
-        <header>
-            <h1>Mark Larah</h1>
-            <aside>
-                <i>Developer of fine artisanal software</i>
-            </aside>
-        </header>
+    <div>
+        <Container>
+            <header>
+                <h1>Mark Larah</h1>
+                <aside>
+                    <i>Developer of fine artisanal software</i>
+                </aside>
+            </header>
 
-        <h2>48656C6C6F20546865726521</h2>
-        <p>
-            I am a Software Engineer from the UK currently working on web
-            infrastructure at <Link href="https://www.yelp.com">Yelp</Link>.
-        </p>
+            <h2>48656C6C6F20546865726521</h2>
+            <p>
+                I am a Software Engineer from the UK currently working on web
+                infrastructure at <Link href="https://www.yelp.com">Yelp</Link>.
+            </p>
 
-        <h2>More Info</h2>
-        <ul>
-            <li>
-                <Link href="https://blog.larah.me" />
-            </li>
-            <li>
-                <Link href="https://uk.linkedin.com/in/marklarah" />
-            </li>
-            <li>
-                <Link href="https://twitter.com/mark_larah" />
-            </li>
-            <li>
-                <Link href="https://github.com/magicmark" />
-            </li>
-        </ul>
+            <h2>More Info</h2>
+            <ul>
+                <li>
+                    <Link href="https://blog.larah.me" />
+                </li>
+                <li>
+                    <Link href="https://uk.linkedin.com/in/marklarah" />
+                </li>
+                <li>
+                    <Link href="https://twitter.com/mark_larah" />
+                </li>
+                <li>
+                    <Link href="https://github.com/magicmark" />
+                </li>
+            </ul>
 
-        <h2>Projects</h2>
-        <ul>
-            <li>
-                <Link href="https://composerize.com/">Composerize</Link>
-            </li>
-            <li>
-                <Link href="https://github.com/sharkcore/tweenz">Tweenz</Link>
-            </li>
-        </ul>
-        <Auth />
-    </Container>
+            <h2>Projects</h2>
+            <ul>
+                <li>
+                    <Link href="https://composerize.com/">Composerize</Link>
+                </li>
+                <li>
+                    <Link href="https://github.com/sharkcore/tweenz">
+                        Tweenz
+                    </Link>
+                </li>
+            </ul>
+            <Auth />
+        </Container>
+        <CookieNotice />
+    </div>
 );
 
 export default Mark;
